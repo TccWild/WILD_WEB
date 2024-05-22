662c8d5b1125f9b82cc974f24d9f407f62eb4b40
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2024-05-22 10:54:25 -0300

    atualização - php

---
+++ view/blades/navbar.php
@@ -0,0 +1,43 @@
+<?php include("blades/top.php");?>
+
+<!--Navbar-->
+<nav>
+    <a href="#" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+
+    <!--Menu Icon-->
+
+    <!--Links-->
+    <ul class="navbar karla-font">
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#Sobre_Nos">Sobre nós</a></li>
+        <li><a href="#Projeto">Projeto</a></li>
+        <li><a href="#Integrantes">Integrantes</a></li>
+    </ul>
+</nav>
+
+<!--Navbar
+<nav class="navbar navbar-expand-sm nav-text karla-font">
+    <div class="container-fluid ">
+        <img src="images/logo.png" width="60" height="60" alt="Logo_WILD">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" style="color:#FFFFFF;" aria-current="page" href="#sobre-nos">Sobre nós</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" style="color:#FFFFFF;" href="#projeto">Projeto</a>
+                </li>			
+                <li class="nav-item">
+                    <a class="nav-link active" style="color:#FFFFFF;" href="#integrantes">Integrantes</a>
+                </li>	
+            </ul>		  
+        </div>
+    </div>
+</nav>
+
+<?php include("blades/top.php");?>
---
+++ view/index.php
@@ -0,0 +1,27 @@
+<?php include("blades/navbar.php"); ?>
+<?php include("blades/header.php"); ?>
+
+
+<!--
+
+<section>
+    <h3>Sobre <span style="color: #EDA011;">nós</span></h3>
+        <div class="container rounded mt-5" style="background-color: #313131;">
+            <div class="row">
+                <div class="col-6 mt-3 text-center">
+                    <img src="images/imagem1.png" class="img-fluid" height="450" width="450">
+                </div>
+                <div class="col-6 mt-5 mb-5 karla-font text-start">
+                    <p style="align-items: left; color: #FFFFFF; font-size: min(20px, 3vw);">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta libero vel tempora quisquam ut dolor non nostrum consequatur reiciendis, dignissimos laboriosam iure iusto doloribus debitis voluptatum blanditiis, harum maxime optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, cupiditate doloribus asperiores laudantium facere quibusdam ipsam voluptas? Sit cupiditate itaque ea accusamus eius, quisquam quas repellendus amet, deleniti voluptates excepturi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum aliquid eveniet explicabo fugit, provident dolorum odio debitis officiis natus quos a, modi nisi quis rem soluta ipsa, distinctio sit magni.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <p style="align-items: left; color: #FFFFFF; font-size: min(20px, 3vw);">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta libero vel tempora quisquam ut dolor non nostrum consequatur reiciendis, dignissimos laboriosam iure iusto doloribus debitis voluptatum blanditiis, harum maxime optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, cupiditate doloribus asperiores laudantium facere quibusdam ipsam voluptas? Sit cupiditate itaque ea accusamus eius, quisquam quas repellendus amet, deleniti voluptates excepturi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum aliquid eveniet explicabo fugit, provident dolorum odio debitis officiis natus quos a, modi nisi quis rem soluta ipsa, distinctio sit magni.</p>
+        </div>
+</section>
+
+<?php include("blades/footer.php"); ?>
+
+    
